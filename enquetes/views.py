1a9f88bed2fdf884dd1cf366cd143521846f3f5f
Francisco Michael <michael.soarez.07@gmail.com>
2020-08-28 19:21:17 -0300

    Criacao de uma pagina dinamica

--- enquetes/views.py
+++ enquetes/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from enquetes.models import Enquete
 
 def bemvindo(request):
     return render(request, 'bemvindo.html')
 
-# Create your views here.
+def enquete(request, enquete_id):
+    enquete = Enquete()
+
+    if enquete_id == 1:
+        enquete = Enquete(1, 'Qual linguagem é mais fácil: JAVA, PHP ou C#?', '28/08/2020')
+    if enquete_id == 2:
+        enquete = Enquete(2, 'Quais áreas de TI tem retorno financeiro mais rápido?', '29/08/2020')
+    if enquete_id == 3:
+        enquete = Enquete(3, 'Qual linguagem é mais fácil: JAVA, PHP ou C#?', '30/08/2020')
+
+    return render(request, 'enquete.html', {"enquete" : enquete})
