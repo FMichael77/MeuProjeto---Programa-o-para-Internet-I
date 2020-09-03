ec7685b6b75c9f91a33eadcd4772d2aea9c779b1
Francisco Michael <michael.soarez.07@gmail.com>
2020-09-03 20:55:01 -0300

    Usando models

--- enquetes/admin.py
+++ enquetes/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from enquetes.models import Enquete
 
-# Register your models here.
+admin.site.register(Enquete)
--- enquetes/views.py
+++ enquetes/views.py
@@ -5,13 +5,5 @@ def bemvindo(request):
     return render(request, 'bemvindo.html')
 
 def enquete(request, enquete_id):
-    enquete = Enquete()
-
-    if enquete_id == 1:
-        enquete = Enquete(1, 'Qual linguagem é mais fácil: JAVA, PHP ou C#?', '28/08/2020')
-    if enquete_id == 2:
-        enquete = Enquete(2, 'Quais áreas de TI tem retorno financeiro mais rápido?', '29/08/2020')
-    if enquete_id == 3:
-        enquete = Enquete(3, 'Qual linguagem é mais fácil: JAVA, PHP ou C#?', '30/08/2020')
-
-    return render(request, 'enquete.html', {"enquete" : enquete})
+    enquete = Enquete.objects.get(id=enquete_id)
+    return render(request, 'enquete.html', {"enquete" : enquete,})
